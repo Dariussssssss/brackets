558f7fc88abbc5e08c428ddb2aac5a77ff83f855
Dariusssssssss <sablezzubaya@gmail.com>
2024-10-15 22:17:18 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports =
+    function check(str, bracketsConfig) {
+        if (str.length % 2 !== 0) return false;
+        let string = '';
+        for (let i = 0; i < str.length; i++) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (str[i] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1] && string[string.length - 1] === bracketsConfig[j][0]) {
+                    string = string.slice(0, -1)
+                } else if (str[i] === bracketsConfig[j][0]) {
+                    string += str[i];
+                } else if (str[i] === bracketsConfig[j][1] && string[string.length - 1] === bracketsConfig[j][0]) {
+                    string = string.slice(0, -1)
+                }
+            }
+        }
+        return string.length === 0
+    }
